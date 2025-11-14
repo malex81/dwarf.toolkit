04e3dd76626d8ba22873a81c29e8198e32e6b4f0
Maxim <malex81@yandex.ru>
2025-11-14 14:20:57 +0300

    add attribute comment

--- Dwarf.Toolkit.Maui/Dwarf.Toolkit.Maui/BindablePropertyAttribute.cs
+++ Dwarf.Toolkit.Maui/Dwarf.Toolkit.Maui/BindablePropertyAttribute.cs
@@ -5,8 +5,18 @@ public sealed class BindablePropertyAttribute : Attribute
 {
 	public object? DefaultValue { get; set; }
 	public string? DefaultValueExpression { get; set; }
+	/// <summary>
+	/// The neme of method for handle prperty changing.
+	/// If not set, a method On<PropertyName>Changing will be generated
 	public string? ChangingMethod { get; set; }
+	/// <summary>
+	/// The neme of method for handle prperty changed.
+	/// If not set, a method On<PropertyName>Changed will be generated
 	public string? ChangedMethod { get; set; }
+	/// <summary>
+	/// The neme of method for validate value.
 	public string? ValidateMethod { get; set; }
+	/// <summary>
+	/// The neme of method for coerce value.
 	public string? CoerceMethod { get; set; }
 }
